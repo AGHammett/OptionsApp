78840c85c591ad2ea2e83063ece6d796172892d5
maxjo <maxjo@MAXZEPHYRUS2>
2024-12-21 13:01:02 +0000

    Add project files.

---
+++ OptionsPricer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OptionsPricer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
